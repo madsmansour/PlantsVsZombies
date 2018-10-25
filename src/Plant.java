de931666106fe26aece9ce1b105ffde93917366d
madsmansour <42993941+madsmansour@users.noreply.github.com>
2018-10-25 12:56:37 +0200

    Initial commit

---
+++ src/Plant.java
@@ -0,0 +1,10 @@
+public class Plant {
+
+    int hpplant = 100;
+    int dmg = 100;
+
+    public Plant(int hpplant, int dmg) {
+        this.hpplant = hpplant;
+        this.dmg = dmg;
+    }
+}
---
+++ src/Zombie.java
@@ -0,0 +1,11 @@
+public class Zombie {
+
+    int hpzombie = 100;
+    int dmgzombie = 10;
+
+    public Zombie(int hpzombie, int dmgzombie) {
+        this.hpzombie = hpzombie;
+        this.dmgzombie = dmgzombie;
+    }
+}
+
